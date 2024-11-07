9c74e009027c00aaa2a2841d9007b766858e7136
pallavidarade2000 <159040775+pallavidarade2000@users.noreply.github.com>
2024-11-07 13:22:54 +0530

    Add files via upload

---
+++ SumOfNumber.java
@@ -0,0 +1,38 @@
+package simpleProgram;
+import java.util.Scanner;
+
+public class SumOfNumber {
+
+	public static void main(String[] args) {
+		
+	/*	int a=30;
+		int b=50;
+		
+			int sum=a+b;
+		
+		System.out.println(sum );
+		}*/
+		
+		    Scanner input = new Scanner(System.in);
+		    
+		
+		    System.out.print("Input the first number: ");
+		    
+		   
+		    int num1 = input.nextInt();
+		    
+		   
+		    System.out.print("Input the second number: ");
+		    
+		    
+		    int num2 = input.nextInt();
+		    
+		  
+		    int sum = num1 + num2;
+		   
+		    System.out.println("Sum: " + sum);
+		  }
+		}
+
+
+
