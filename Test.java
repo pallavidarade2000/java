e55271cfa03e40a56cff81f66dadf1cdd28b856d
pallavidarade2000 <159040775+pallavidarade2000@users.noreply.github.com>
2024-11-19 13:28:07 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package collections;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Object[] obj= {101,"Pallavi",50000.00};
+		System.out.println(obj[0]);
+		System.out.println(obj[1]);
+		System.out.println(obj[2]);
+		
+
+	}
+
+}
