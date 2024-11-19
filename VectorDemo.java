a0b91a84d4bdb992fac824d3779e6c88d1fd43c0
pallavidarade2000 <159040775+pallavidarade2000@users.noreply.github.com>
2024-11-19 13:29:27 +0530

    Add files via upload

---
+++ VectorDemo.java
@@ -0,0 +1,24 @@
+package collections;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		
+		Vector<Integer> v=new Vector<Integer>();
+		
+		v.add(100);
+		v.add(200);
+		v.add(300);
+		v.add(400);
+		v.add(500);
+		
+		System.out.println(v);
+		
+	
+		
+		
+
+	}
+
+}
